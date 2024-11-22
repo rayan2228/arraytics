3387361d968598fb645b81e74e7e1de86ac899f7
Rayan Hossain <80379977+rayan2228@users.noreply.github.com>
2024-11-22 09:31:31 +0600

    make few style components like grid ,flex, button ,title

--- problem 1/src/styleComponent/index.js
+++ problem 1/src/styleComponent/index.js
@@ -4,4 +4,32 @@ const Container = styled.div`
     margin:auto
 `;
 
-export { Container }
+const Grid = styled.div`
+    display:grid,
+    grid-template-columns:${({ templateColumns }) => templateColumns};
+    grid-template-rows:${({ templateRows }) => templateRows};
+    gap:${({ gap }) => gap};
+`
+const Flex = styled.div`
+    display:flex;
+    align-items:${({ alignItems }) => alignItems};
+    justify-content:${({ justifyContent }) => justifyContent};
+    flex-wrap:${({ flexWrap }) => flexWrap};
+    gap:${({ gap }) => gap};
+    flex-direction:${({ flexDirection }) => flexDirection};
+`
+const Button = styled.button`
+    background-color:${({ backgroundColor }) => backgroundColor};
+    color:${({ color }) => color};
+    border-radius:${({ borderRadius }) => borderRadius};
+    padding:${({ padding }) => padding};
+    border:${({ border }) => border};
+`
+const Title = styled.h2`
+    font-size:${({ fontSize }) => fontSize};
+    color:${({ color }) => color};
+    font-weight:${({ fontWeight }) => fontWeight};
+    text-align:${({ textAlign }) => textAlign};
+`
+
+export { Container, Grid, Flex, Button, Title }
